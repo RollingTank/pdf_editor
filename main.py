2ce3b378dd02aae27f86abb5fa4c33f0deb7555a
Tanush K <kultanush@gmail.com>
2025-07-22 22:41:44 -0400

    added append by folder and reorder pages in pdf

--- main.py
+++ main.py
@@ -9,6 +9,13 @@ def append_files(*files: str, new_name: str):
         new_file.append(file)
     new_file.write("f{new_name}.pdf")
 
+def append_files_folder(folder: str, new_name: str):
+    nf = pypdf.PdfWriter(incremental=True)
+    for file in os.listdir(folder):
+        pth = os.path.join(folder, file)
+        nf.append(pth)
+    nf.write(f"{new_name}.pdf")
+
 def remove_pages(pgs: List, file: str, new_name: str):
     f = pypdf.PdfWriter(incremental=True)
     j = 1
@@ -17,6 +24,11 @@ def remove_pages(pgs: List, file: str, new_name: str):
             f.append(i)
     f.write("f{new_name}.pdf")
 
-
+def reorder_pages(pgs: List, file: str, new_name: str):
+    r = pypdf.PdfReader(file)
+    nf = pypdf.PdfWriter(incremental=True)
+    for i in range(0, len(pgs)):
+        nf.append(r.pages[i])
+    nf.write(f"{new_name}.pdf")
 
 
